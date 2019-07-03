fed91c97f82ea746959b0178c24c221716ef9b44
cooperrholmes@gmail.com <cooperrholmes@gmail.com>
2019-07-03 07:19:49 +0000

    Added enrollment feature, enhanced site permissions and modified database structure

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!  
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   end
@@ -6,6 +8,12 @@ class LessonsController < ApplicationController
 
 
   private
+
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You are not enrolled in this course."
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
